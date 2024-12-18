a1a58c6c1694b1106a2a731af9b136777d5acba9
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-12-18 17:05:29 +0000

    Day 2 - find safe reports
    You have to find the correct number of safe reports
    
    - reports must either increment of decrment for the whole sequence
    - reports cannot incrememnt of decrement by more than 3
    - report cannot have the same int twice in a row (covered by the first point)
    - reports are safe if when you remove 1 level (int) and they abide by
    the above rules

--- app/day_two.py
+++ app/day_two.py
@@ -1005,3 +1005,37 @@ class DayTwo:
     def format_report_data(self):
       lines = self.REPORT_DATA.strip().split("\n")
       return [[int(element) for element in line.split()] for line in lines]
+
+    def calculate_safe_reports(self, reports):
+        count = 0
+
+        for report in reports:
+            if self.__is_safe_report(report):
+                count += 1
+            else:
+                if self.__can_be_safe_with_removal(report):
+                    count += 1
+
+        return count
+
+    def __is_safe_report(self, report):
+        return self.__check_report_safe(report, mode="ascending") or \
+               self.__check_report_safe(report, mode="descending")
+
+    def __check_report_safe(self, report, mode="ascending"):
+
+        for i in range(len(report) - 1):
+            current_int = report[i]
+            next_int = report[i + 1]
+            if (mode == "ascending" and (next_int <= current_int or next_int > current_int + 3)) or \
+               (mode == "descending" and (next_int >= current_int or next_int < current_int - 3)):
+                return False
+
+        return True
+
+    def __can_be_safe_with_removal(self, report):
+        for i in range(len(report)):
+            modified_report = report[:i] + report[i + 1:]
+            if self.__is_safe_report(modified_report):
+                return True
+        return False
--- tests/day_two_test.py
+++ tests/day_two_test.py
@@ -1,7 +1,58 @@
 import pytest
 from app.day_two import DayTwo
 
-def test_format_report_data_returns_nested_arrays_of_ints():
+def test_format_report_data_returns_1000_reports_of_nested_arrays_of_ints():
   formatted_data = DayTwo().format_report_data()
   assert len(formatted_data) == 1000
   assert all(isinstance(el, int) for el in formatted_data[0])
+
+def test_calculate_safe_reports_identifies_incrementing_reports_as_safe():
+  reports = [[1,2,5,6,7]]
+  assert DayTwo().calculate_safe_reports(reports) == 1
+
+def test_calculate_safe_reports_identifies_decrementing_reports_as_safe():
+  reports = [[8,7,6,4,2]]
+  assert DayTwo().calculate_safe_reports(reports) == 1
+
+def test_calculate_safe_reports_identifies_reports_with_identical_consecutive_ints_as_safe():
+  reports = [[1,1,2,4,5]]
+  assert DayTwo().calculate_safe_reports(reports) == 1
+
+def test_calculate_safe_reports_identifies_reports_with_multiple_identical_consecutive_ints_as_unsafe():
+  reports = [[1,1,2,2,4,5]]
+  assert DayTwo().calculate_safe_reports(reports) == 0
+
+def test_calculate_safe_reports_doesnt_count_reports_that_increment_by_more_than_three_as_safe():
+  reports = [[1,5,6]]
+  assert DayTwo().calculate_safe_reports(reports) == 1
+
+def test_calculate_safe_reports_identifies_reports_that_increment_by_more_than_three_twice_as_unsafe():
+    reports = [[1,5,9]]
+    assert DayTwo().calculate_safe_reports(reports) == 0
+
+def test_calculate_safe_reports_doesnt_count_reports_that_decrement_by_more_than_three_as_safe():
+  reports = [[6,5,1]]
+  assert DayTwo().calculate_safe_reports(reports) == 1
+
+def test_calculate_safe_reports_identifies_reports_that_decrement_by_more_than_three_twice_as_unsafe():
+    reports = [[9,5,1]]
+    assert DayTwo().calculate_safe_reports(reports) == 0
+
+def test_calculate_safe_reports_gets_the_correct_answer_when_using_report_data():
+  reports = DayTwo().format_report_data()
+  assert DayTwo().calculate_safe_reports(reports) == 311
+
+def test_calculate_safe_reports_two_unsafe_levels_are_unsafe():
+  reports1 = [[7, 6, 4, 2, 1]]
+  reports2 = [[1, 2, 7, 8, 9]]
+  reports3 = [[9, 7, 6, 2, 1]]
+  reports4 = [[1, 3, 2, 4, 5]]
+  reports5 = [[8, 6, 4, 4, 1]]
+  reports6 = [[1, 3, 6, 7, 9]]
+
+  assert DayTwo().calculate_safe_reports(reports1) == 1
+  assert DayTwo().calculate_safe_reports(reports2) == 0
+  assert DayTwo().calculate_safe_reports(reports3) == 0
+  assert DayTwo().calculate_safe_reports(reports4) == 1
+  assert DayTwo().calculate_safe_reports(reports5) == 1
+  assert DayTwo().calculate_safe_reports(reports6) == 1
