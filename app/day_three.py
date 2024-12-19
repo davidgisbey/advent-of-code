9c88952d523e7d729d580a232a921d027ddb8925
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-12-19 13:31:48 +0000

    Day 3 part 2

--- app/day_three.py
+++ app/day_three.py
@@ -2,7 +2,12 @@ import re
 
 class DayThree:
     def sum_multiplications_in_data(self, data):
-      pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+      pattern = r"^[^d]*(?=do\(\)|don't\(\))|(?<=do\(\))[\s\S]*?(?=don't\(\))|(?<=do\(\))[\s\S]*"
       matches = re.findall(pattern, data)
-      return sum(int(x) * int(y) for x, y in matches)
+      mul_pattern = r"mul\((\d{1,3}),(\d{1,3})\)"
+      total_sum = 0
+      for match in matches:
+          mul_matches = re.findall(mul_pattern, match)
+          total_sum += sum(int(x) * int(y) for x, y in mul_matches)
 
+      return total_sum
--- tests/day_three_test.py
+++ tests/day_three_test.py
@@ -3,9 +3,17 @@ from app.day_three import DayThree
 from app.data.day_three_corrupted_data import DayThreeCorruptedData
 
 def test_sum_multiplications_in_data_matches_multiply_with_up_to_three_digits_and_sums_them():
-  data = "acmul(1,120)adad1!mul12345mul(100,5)adadad!$mul(10,4000)"
+  data = "do()acmul(1,120)adad1!mul12345mul(100,5)adadad!$mul(10,4000)don't()"
+  assert DayThree().sum_multiplications_in_data(data) == 620
+
+def test_sum_multiplications_in_data_only_captures_muls_between_do_and_donts():
+  data = "do()mul(1,120)don't() mul(1,20) do()mul(100,5)don't() do()mul(10,4000)don't()"
+  assert DayThree().sum_multiplications_in_data(data) == 620
+
+def test_sum_multiplications_in_data_works_if_there_is_not_a_dont_function():
+  data = "do()acmul(1,120)adad1!mul12345mul(100,5)adadad!$mul(10,4000)"
   assert DayThree().sum_multiplications_in_data(data) == 620
 
 def test_sum_multiplications_in_data_works_with_provided_data():
   data = DayThreeCorruptedData.CORRUPTED_DATA
-  assert DayThree().sum_multiplications_in_data(data) == 188116424
+  assert DayThree().sum_multiplications_in_data(data) == 104245808
